f0cc507906f0bc997b418d2f97ce856874118260
fkdkskdfjfk <yljw225@naver.com>
2024-07-23 16:04:22 +0900

    trade route

---
+++ src/routes/trade.ts
@@ -0,0 +1,17 @@
+import express from "express";
+
+const router = express.Router();
+
+router.post(``, async (req,res,next)=>{
+    try {
+
+
+      
+    } catch (err) {
+      console.error(err);
+    }
+  })
+
+
+
+module.exports = router;
