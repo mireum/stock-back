43760e123763bbb9c9399ee3976d47af489ca291
fkdkskdfjfk <yljw225@naver.com>
2024-08-02 21:10:08 +0900

    ownedStock 초기화

--- src/routes/trade.ts
+++ src/routes/trade.ts
@@ -8,18 +8,13 @@ router.post(`/buystock`, async (req,res,next)=>{
     const { form, kakaoId } = req.body;
     const { Stockname, price, stockNumber } = form;
     const stockInfo = JSON.stringify({ Stockname, price, stockNumber });
-    console.log(form);
-    console.log(stockInfo);
-    console.log(kakaoId);
-    
     
     // mysql에 구매한 주식 정보 저장
+    // ownedStock이 null이면 빈 배열로 초기화
     const nullsql = `UPDATE user SET ownedStock = IFNULL(ownedStock, JSON_ARRAY()) WHERE id = ?`;
     const nullresult = await handleSql(nullsql, kakaoId);
     const sql = `UPDATE user SET ownedStock = JSON_ARRAY_APPEND(ownedStock, '$', CAST(? AS JSON)) WHERE id = ?`;
-    const result = await handleSql(sql, [stockInfo, kakaoId]);
-    console.log(`주식세이브`, result);
-    
+    const result = await handleSql(sql, [stockInfo, kakaoId]);    
 
     res.json({
       message: '주식 구매 성공',
