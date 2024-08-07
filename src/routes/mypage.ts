fa7cd84726d7ff6072194e2ec2fce74463a11fe6
fkdkskdfjfk <yljw225@naver.com>
2024-08-07 23:17:26 +0900

    주식 매매 완료

--- src/routes/mypage.ts
+++ src/routes/mypage.ts
@@ -24,6 +24,34 @@ router.post(`/myStock`, async (req,res,next)=>{
   }
 });
 
+router.post(`/deleteStock`, async (req,res,next)=>{
+  const { kakaoId, stockname } = req.body;
+  try {
+    const selectSql = "SELECT ownedstock FROM user WHERE id = ?";
+    const userData = await handleSql(selectSql, [kakaoId]);
+    let ownedstock = userData[0].ownedstock;
+    ownedstock = ownedstock.filter((stock:any) => stock.stockname !== stockname);
+    const updateSql = "UPDATE user SET ownedstock = ? WHERE id = ?";
+    const data = await handleSql(updateSql, [JSON.stringify(ownedstock), kakaoId]);
+    console.log(data);
+    // if (data[0].ownedstock) {
+    //   res.json({
+    //     flag: true,
+    //     data: data[0].ownedstock
+    //   });
+    // } else {
+      // res.json({
+      //   flag: false,
+      // });
+    // }
+    res.json({
+      flag: true,
+    })
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 // const sql = "DELETE ownedstock FROM user WHERE id = ?";
 
 module.exports = router;
